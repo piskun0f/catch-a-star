8d4689ce319b7fd2816cea160d5465e5732a5e26
Андрей Пискунов <andrepisk2000@gmail.com>
2021-09-26 19:38:31 +0300

    ✨ Add gamecounter

--- Assets/Scripts/GameCounterScript.cs
+++ Assets/Scripts/GameCounterScript.cs
@@ -1,21 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameCounterScript : MonoBehaviour
 {
-    private int count = 0;
+    [SerializeField] Text text;
+    int count = 0;
 
     public int AddCount(int addNum = 1)
     {
         count += addNum;
-        
+        ChangeText();
         return count;
     }
-
     public static GameCounterScript operator ++(GameCounterScript gameCounter)
     {
         gameCounter.AddCount();
         return gameCounter;
     }
+
+    void ChangeText()
+    {
+        text.text = count.ToString();
+    }
+
 }
